c7c0bd65e1ad940417a2d23e1db2663a2d4125e9
CHOCOJ <daniel239cruz@gmail.com>
2024-05-20 11:00:15 -0600

    SE AGRGÓ UN NAVBAR

---
+++ vistas/templates/navbar.php
@@ -0,0 +1,18 @@
+<?php include_once '../templates/header.php'; ?>
+
+<nav class="navbar fixed-top navbar-expand-lg bg-success bg-opacity-75 justify-content-center">
+  <div class="container-fluid justify-content-center">
+    <a class="navbar-brand align-items-center" href="#">
+      <img src="/src/images/c.png" alt="Logo" width="60" height="60">
+      <span class="mx-2">APLICACIÓN DE CONTROL DE RANCHO DEL COMANDO DE INFORMÁTICA Y TECNOLOGIA</span>
+      <img src="/src/images/c.png" alt="Logo" width="60" height="60">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
+
+
+
+<?php include_once '../templates/footer.php'; ?>
